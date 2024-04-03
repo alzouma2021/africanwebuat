7b53aa61fe825794affa0a8874b3fbbcb1df657a
alzouma2021 <86126887+alzouma2021@users.noreply.github.com>
2024-04-03 10:10:01 +0000

    Update vue.config.js

--- africanb/vue.config.js
+++ africanb/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    allowedHosts: ['africanweb-26408e5071aa.herokuapp.com'], 
+  },
   transpileDependencies: [
     'vuetify'
   ],
